6ca6625c692c869672b1affa38ff274e6f8d7f1a
zecaidam <zecaidam@gmail.com>
2015-06-30 16:53:26 -0600

    Version1(Tarea3)

---
+++ Alfax.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#chkconfig: 2345 02 03
+#Descripcion: Es un daemon!
+
+case "$1" in
+	start)
+		echo -n "::: Iniciando daemon Alfax :::"
+		fecha='date'
+		echo $fecha >>  /tmp/Alfax/despliegue
+		exit 0
+		;;
+	stop)
+		echo -n "::: Parando daemon Alfax :::"
+		rm -f /tmp/Alfax/despliegue
+		exit 0
+		;;
+	*)
+		echo $"Usage$0(start|stop)"
+		exit 2
+esac
+
+
